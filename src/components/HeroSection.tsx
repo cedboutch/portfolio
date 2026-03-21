ad5019ef1b1cf0f3a68eee6166d1105a167b3514
Cédric <cedric.bouchard.4@ulaval.ca>
2026-03-21 00:04:18 -0400

    change hero image

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import styles from './HeroSection.module.css';
-import heroImg from '../assets/HeroImage.png';
+import heroImg from '../assets/HeroImage.jpg';
 
 const HeroSection: React.FC = () => {
     return (
         <section id="about" className={styles.heroContainer}>
             <div className={styles.heroContent}>
-                <h1 className={styles.greeting}>Yo!</h1>
-                <h2 className={styles.title}>Cédric Bouchard</h2>
+                <h1 className={styles.greeting}>Cédric Bouchard</h1>
                 <p className={styles.description}>
                     Je suis un étudiant en génie logiciel et je suis passionné par le développement de logiciels et l'intelligence artificielle.
                 </p>
