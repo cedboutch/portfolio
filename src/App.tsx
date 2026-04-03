55905a34587fdecb45c49e0d5d85b9c4ae4ffcae
Cédric <cedric.bouchard.4@ulaval.ca>
2026-04-02 22:36:22 -0400

    grid animation

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,18 @@ const ResumeSection = React.lazy(() => import('./components/ResumeSection'))
 const ProjectsSection = React.lazy(() => import('./components/ProjectsSection'))
 
 function App() {
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    e.currentTarget.style.setProperty('--mouse-x', `${x}px`);
+    e.currentTarget.style.setProperty('--mouse-y', `${y}px`);
+  };
+
   return (
     <div>
+      <div className="side-grid side-grid-left" onMouseMove={handleMouseMove}></div>
+      <div className="side-grid side-grid-right" onMouseMove={handleMouseMove}></div>
       <Navbar />
       <main className="main-content">
         <HeroSection />
